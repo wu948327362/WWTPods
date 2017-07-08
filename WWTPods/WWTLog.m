8ed039435ec8c19c955f09233b05038783ed62d6
wuwentao <wuwentao@qiyi.com>
2017-07-08 18:27:49 +0800

    add files

---
+++ WWTPods/WWTLog.m
@@ -0,0 +1,15 @@
+//
+//  WWTLog.m
+//  WWTPods
+//
+//  Created by wuwentao on 17/7/8.
+//  Copyright © 2017年 wuwentao. All rights reserved.
+//
+
+#import "WWTLog.h"
+
+@implementation WWTLog
+- (void)logWWT{
+    NSLog(@"this is log");
+}
+@end
---
+++ WWTPodsLib/WWTLog.h
@@ -0,0 +1,13 @@
+//
+//  WWTLog.h
+//  WWTPods
+//
+//  Created by wuwentao on 17/7/8.
+//  Copyright © 2017年 wuwentao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WWTLog : NSObject
+- (void)logWWT;
+@end
